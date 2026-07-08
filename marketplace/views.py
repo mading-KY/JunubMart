d97d214eee28ade10d163a86fdff160e0f0a1a2f
mading-KY <madingmajok484@gmail.com>
2026-07-08 15:00:52 +0300

    Updated

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def add_product(request):
+    return render(request, "marketplace/add_product.html")
+
+def product_list(request):
+    return render(request, "marketplace/product_list.html")
+
+def product_detail(request, pk):
+    return render(request, "marketplace/product_detail.html")
+
+
